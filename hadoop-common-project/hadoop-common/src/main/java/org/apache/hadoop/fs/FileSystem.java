400fbf2a2992317723bd0ae8c0972c0a03381cbe
Bryan Beaudreault <bbeaudreault@hubspot.com>
2024-04-22 13:45:04 -0400

    HubSpot Backport: MAPREDUCE-6660: Add MR Counters for bytes-read-by-network-distance FileSystem metrics (not yet merged upstream)

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/FileSystem.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/FileSystem.java
@@ -3961,7 +3961,7 @@ public abstract class FileSystem extends Configured
       /**
        * Add another StatisticsData object to this one.
        */
-      void add(StatisticsData other) {
+      public void add(StatisticsData other) {
         this.bytesRead += other.bytesRead;
         this.bytesWritten += other.bytesWritten;
         this.readOps += other.readOps;
@@ -4037,6 +4037,15 @@ public abstract class FileSystem extends Configured
 
       public long getBytesReadDistanceOfFiveOrLarger() {
         return bytesReadDistanceOfFiveOrLarger;
+      }
+
+      public boolean hasNetworkDistanceData() {
+        return (
+                bytesReadLocalHost > 0 ||
+                bytesReadDistanceOfOneOrTwo > 0 ||
+                bytesReadDistanceOfThreeOrFour > 0 ||
+                bytesReadDistanceOfFiveOrLarger > 0
+                );
       }
 
       public long getBytesReadErasureCoded() {
