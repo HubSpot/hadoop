c1648811609fb13438f66756a71c6a4fc39fcb01
Charles Connell <charles@connells.org>
2024-12-27 16:43:56 -0500

    HubSpot Backport: HDFS-17685: Option to explicitly choose DFS client lease renewal interval (not yet merged upstream)

--- hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/client/impl/DfsClientConf.java
+++ hadoop-hdfs-project/hadoop-hdfs-client/src/main/java/org/apache/hadoop/hdfs/client/impl/DfsClientConf.java
@@ -107,6 +107,7 @@ public class DfsClientConf {
                                                                 .class);
 
   private final int hdfsTimeout;    // timeout value for a DFS operation.
+  private final int leaseRenewalIntervalMs;
 
   private final int maxFailoverAttempts;
   private final int maxRetryAttempts;
@@ -172,7 +173,9 @@ public class DfsClientConf {
   public DfsClientConf(Configuration conf) {
     // The hdfsTimeout is currently the same as the ipc timeout
     hdfsTimeout = Client.getRpcTimeout(conf);
-
+    leaseRenewalIntervalMs = conf.getInt(
+        HdfsClientConfigKeys.DFS_CLIENT_LEASE_RENEWAL_INTERVAL_KEY,
+        HdfsClientConfigKeys.DFS_CLIENT_LEASE_RENEWAL_INTERVAL_DEFAULT);
     maxRetryAttempts = conf.getInt(
         Retry.MAX_ATTEMPTS_KEY,
         Retry.MAX_ATTEMPTS_DEFAULT);
@@ -185,7 +188,6 @@ public class DfsClientConf {
     retryIntervalForGetLastBlockLength = conf.getInt(
         Retry.INTERVAL_GET_LAST_BLOCK_LENGTH_KEY,
         Retry.INTERVAL_GET_LAST_BLOCK_LENGTH_DEFAULT);
-
     maxFailoverAttempts = conf.getInt(
         Failover.MAX_ATTEMPTS_KEY,
         Failover.MAX_ATTEMPTS_DEFAULT);
@@ -448,6 +450,10 @@ public class DfsClientConf {
    */
   public int getHdfsTimeout() {
     return hdfsTimeout;
+  }
+
+  public int getLeaseRenewalIntervalMs() {
+    return leaseRenewalIntervalMs;
   }
 
   /**
